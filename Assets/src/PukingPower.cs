d69deb4d7a6a8e2b71ac874acae9be4eac34e216
andrew_ishchuk <andrew_ishchuk@office.targem.ru>
2015-04-18 19:08:09 +0500

    pukan
    Conflicts:
    	Assets/scene.unity

---
+++ Assets/src/PukingPower.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PukingPower : MonoBehaviour {
+
+	public float emissionMod = 1.0f;
+	public float speedMod = 1.0f;
+
+    private float baseEmissionRate;
+    private float baseParticleSpeed;
+
+    void Start()
+    {
+        ParticleSystem ps = GetComponent<ParticleSystem>() as ParticleSystem;
+        baseEmissionRate = ps.emissionRate;
+        baseParticleSpeed = ps.startSpeed;
+    }
+
+	public void ApplyThrust( Vector2 power )
+	{
+        float powerF = power.magnitude;
+
+        ParticleSystem ps = GetComponent<ParticleSystem>() as ParticleSystem;
+
+        float angle = Mathf.Atan2(-power.x, power.y) * Mathf.Rad2Deg;
+        ps.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        ps.emissionRate = baseEmissionRate * powerF * emissionMod;
+        ps.startSpeed = baseParticleSpeed * powerF * speedMod;
+	}
+}
