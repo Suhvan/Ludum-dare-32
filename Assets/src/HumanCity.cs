b8f158076a7b86d9bc9e7614940bcd5f004033d9
qwer <suhvan4ik@gmail.com>
2015-04-18 15:20:45 +0500

    cityes

---
+++ Assets/src/HumanCity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class HumanCity : MonoBehaviour
+{
+    public float rocketSpawnCD;
+    public float rocketSpawnClock;
+
+    public GameObject RocketPref;
+    public GameObject SpawnPoint;   
+
+    void Update () 
+    {
+        rocketSpawnClock += Time.deltaTime;
+        if (rocketSpawnClock > rocketSpawnCD)
+        {
+            SpawnRocket();
+            rocketSpawnClock = 0;
+        }    
+    }
+
+
+    private void SpawnRocket()
+    {
+        Instantiate(RocketPref, SpawnPoint.transform.position, new Quaternion(0,0,0,0));
+        Debug.Log("Pew-pew");
+    }
+
+
+}
