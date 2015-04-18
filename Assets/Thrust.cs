72f166c7cc7e1dfc3cdc867aa6a0786160a888ee
Artem <artemxaoc@gmail.com>
2015-04-18 13:30:14 +0500

    init commit

---
+++ Assets/Thrust.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Thrust : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+
+    public float thrust = 10;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+
+    void FixedUpdate()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+        rigidBody.AddForce(transform.forward * thrust);
+	}
+}
