27c19aa62823e83b88e86f2643eb17096a04f8d5
Artem <artemxaoc@gmail.com>
2015-04-19 13:12:39 +0500

    ПВО

--- Assets/RocketLauncher.cs
+++ Assets/RocketLauncher.cs
@@ -12,6 +12,8 @@ public class RocketLauncher : MonoBehaviour {
     public GameObject SpawnPoint;
     public GameObject PreRocketSpawnPref;
     public GameObject TargetPoint;
+    public GameObject UFO;
+    public float UFOTargetProbability = 0.1f;
     
 
     public float MinHeight = 2;
@@ -36,29 +38,26 @@ public class RocketLauncher : MonoBehaviour {
         }
     }
 
-    private Vector2 RocketImpulse(Thrust tr)
+    private Vector2 RocketImpulse(Thrust tr, Vector2 targetPos, float DistanceSpread = 0, float MinHeight = 0, float MaxHeight = 0)
     {
 
-        Vector2 targetPos = TargetPoint.transform.position;
+        //Vector2 targetPos = TargetPoint.transform.position;
         Vector2 startPos = SpawnPoint.transform.position;
 
-        float height = Mathf.Abs(MinHeight + Random.value * (MaxHeight - MinHeight)) + Mathf.Min(startPos.y, targetPos.y);
-
-
-        height = Mathf.Max(height, targetPos.y, startPos.y);
-
+        float absoluteHeight = Mathf.Abs(MinHeight + Random.value * (MaxHeight - MinHeight)) + Mathf.Min(startPos.y, targetPos.y);
         float dx = targetPos.x - startPos.x;
-
         float distance = Mathf.Abs(dx) + (Random.value - 0.5f) * DistanceSpread;
+
+        absoluteHeight = Mathf.Max(absoluteHeight, targetPos.y, startPos.y);
 
         float direction = Mathf.Sign(dx);
 
         float gravity = -Physics2D.gravity.y * tr.rigidBody.gravityScale;
 
-        float V0y = Mathf.Sqrt(2 * gravity * (height - startPos.y));
+        float V0y = Mathf.Sqrt(2 * gravity * (absoluteHeight - startPos.y));
         float timeup = V0y / gravity;
 
-        float timedown = Mathf.Sqrt(2 * (height - targetPos.y) / gravity);
+        float timedown = Mathf.Sqrt(2 * (absoluteHeight - targetPos.y) / gravity);
 
         float V0x = distance / (timeup + timedown);
         return new Vector2(V0x * direction, V0y);
@@ -69,7 +68,16 @@ public class RocketLauncher : MonoBehaviour {
         GameObject rocket = Instantiate(RocketPref, SpawnPoint.transform.position, new Quaternion(0, 0, 0, 0)) as GameObject;
         Thrust thrust = rocket.GetComponent<Thrust>();
         thrust.rigidBody.gravityScale = Random.value * gravitySpread + 1;
-        thrust.SetImpulse(RocketImpulse(thrust));
+
+        if (Random.value >= UFOTargetProbability)
+        {
+            thrust.SetImpulse(RocketImpulse(thrust, TargetPoint.transform.position, DistanceSpread, MinHeight, MaxHeight));
+        }
+        else
+        {
+            thrust.SetImpulse(RocketImpulse(thrust, UFO.transform.position));
+        }
+
     }
 
 }
