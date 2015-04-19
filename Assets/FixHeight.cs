be413b2958d9c8ffcad9cb0c92669b9c621b4946
Uvarov A.N <blustersan@gmail.com>
2015-04-19 15:02:53 +0500

    particles stick to ground

---
+++ Assets/FixHeight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixHeight : MonoBehaviour {
+
+	private float fixedHeight;
+
+	void Awake()
+	{
+		fixedHeight = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var oldPos = this.transform.position;
+		oldPos.Set(oldPos.x, fixedHeight, oldPos.z);
+		this.transform.position = oldPos;
+	}
+}
