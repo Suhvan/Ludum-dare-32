d8d42e4109648168d1a0c39345a579785dbe17cb
qwer <suhvan4ik@gmail.com>
2015-04-19 21:34:24 +0500

    cursor fix

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -13,34 +13,30 @@ namespace Assets
 	public class Cursor : MonoBehaviour
 	{
 		[SerializeField]
-		private float altLimit = 10;
+		private float altLimit = 0;
 
-		private Rectangle rect;
+        private Vector3 bottomLeft;
+        private Vector3 topRight;
 
 		void Start()
 		{
-			rect = new Rectangle()
-			{
-				left = 0,
-				top = Screen.height,
-				right = Screen.width,
-				bottom = 0 + altLimit,
-			};
+            bottomLeft = Camera.main.ScreenToWorldPoint( new Vector3( 0, 0, 0) ) + new Vector3(0,altLimit,0);
+            topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));			
 		}
 
-		public Vector3 clampedMouse { get; set; }
+		//public Vector3 clampedMouse { get; set; }
 
 		void Update()
 		{
 			if (GameCore.instance.onPause)
 				return;
 
-			var mousePos = Input.mousePosition;
-			clampedMouse = new Vector3(Mathf.Clamp(mousePos.x, rect.left, rect.right), Mathf.Clamp(mousePos.y, rect.bottom, rect.top), mousePos.z);
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var worldPos = new Vector3(Mathf.Clamp(mousePos.x, bottomLeft.x, topRight.x), Mathf.Clamp(mousePos.y, bottomLeft.y, topRight.y), mousePos.z);
 
             //Debug.Log(mousePos.y);
 
-			var worldPos = Camera.main.ScreenToWorldPoint(clampedMouse);
+			//var worldPos = clampedMouse;
 			worldPos.Set(worldPos.x, worldPos.y, 0);
 			this.transform.position = worldPos;
 		}
