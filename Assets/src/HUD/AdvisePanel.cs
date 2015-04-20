5e6291c92947ff332bfb442344948781841bc845
qwer <suhvan4ik@gmail.com>
2015-04-20 21:30:50 +0500

    кредитсы + советы

---
+++ Assets/src/HUD/AdvisePanel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+class AdvisePanel : MonoBehaviour
+{
+    public Text advice;
+    public int seconds=3;
+
+    private string[] advices = {"Save people! Harvest them with your gravitygun (LMB)",
+        "When rocket hits city, it kills people.",
+        "You are not immortal!",
+        "It's not a cat!",
+        "Redirect rockets using your gravitygun (LMB)",
+        "Red rockets aims at YOU!",
+        "There is gravitygun (LMB) charge at top left corner",
+        "You must save everyone!",
+        "Don't let people die!"};
+
+    void Start()
+    {
+        advice.text = advices[UnityEngine.Random.Range(0, advices.Length - 1)];
+        StartCoroutine(Hider());
+    }
+
+    System.Collections.IEnumerator Hider()
+    {
+        yield return new WaitForSeconds(seconds);
+        gameObject.SetActive(false);     
+    }
+}
+
